2ad0eb0716c57096b1cc52afdd752a30f551ee58
Guilherme Bafica <gb.engcomp@hotmail.com>
2020-08-29 20:58:15 -0300

    CHORE: Estiliza tela de cadastro

--- src/pages/SignIn/styles.ts
+++ src/pages/SignIn/styles.ts
@@ -1,3 +1,4 @@
+import { Platform } from 'react-native';
 import styled from 'styled-components/native';
 import { getBottomSpace } from 'react-native-iphone-x-helper';
 
@@ -5,7 +6,7 @@ export const Container = styled.View`
   flex: 1;
   align-items: center;
   justify-content: center;
-  padding: 0 30px;
+  padding: 0 30px ${Platform.OS === 'android' ? 160 : 40}px;
 `;
 
 export const Title = styled.Text`
@@ -41,7 +42,7 @@ export const CreateAccountButton = styled.TouchableOpacity`
 `;
 
 export const CreateAccountButtonText = styled.Text`
-  color: #fff;
+  color: #ff9000;
   font-size: 18px;
   font-family: 'RobotoSlab-Regular';
   margin-left: 16px;
