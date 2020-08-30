7d4c779b1a1f13a9e9395194c7e0f263f3d5b7be
Guilherme Bafica <gb.engcomp@hotmail.com>
2020-08-30 17:57:35 -0300

    ADD: Cadastra na aplicaçao

--- src/pages/SignUp/index.tsx
+++ src/pages/SignUp/index.tsx
@@ -13,6 +13,7 @@ import { useNavigation } from '@react-navigation/native';
 import { Form } from '@unform/mobile';
 import { FormHandles } from '@unform/core';
 import * as Yup from 'yup';
+import api from '../../services/api';
 
 import getValidationErrors from '../../utils/getValidationErrors';
 
@@ -52,9 +53,14 @@ const SignUp: React.FC = () => {
         abortEarly: false,
       });
 
-      // await api.post('/users', data);
+      await api.post('/users', data);
 
-      // history.push('/');
+      Alert.alert(
+        'Cadastro realizado com sucesso!',
+        'Você já pode fazer login na aplicação.',
+      );
+
+      navigation.navigate('SignIn');
     } catch (err) {
       if (err instanceof Yup.ValidationError) {
         const errors = getValidationErrors(err);
@@ -114,6 +120,7 @@ const SignUp: React.FC = () => {
               <Input
                 ref={passwordInputRef}
                 name="password"
+                secureTextEntry
                 icon="lock"
                 placeholder="Senha"
                 textContentType="newPassword"
