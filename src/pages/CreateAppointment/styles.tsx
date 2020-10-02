14ddb7bdd8dc7c6f0aed85ebcb317d051e68d702
Guilherme Bafica <gb.engcomp@hotmail.com>
2020-10-01 22:28:06 -0300

    CHORE: Cria lista de prestadores na pagina do prestador

--- src/pages/CreateAppointment/styles.tsx
+++ src/pages/CreateAppointment/styles.tsx
@@ -1,5 +1,12 @@
-import { RectButton } from 'react-native-gesture-handler';
 import styled from 'styled-components/native';
+
+interface ProviderContainerPros {
+  selected?: boolean;
+}
+
+interface ProviderNamePros {
+  selected?: boolean;
+}
 
 export const Container = styled.View`
   flex: 1;
@@ -28,4 +35,34 @@ export const UserAvatar = styled.Image`
   height: 56px;
   border-radius: 28px;
   margin-left: auto;
+`;
+
+export const ProvidersListView = styled.View`
+  height: 112px;
+`;
+
+export const ProvidersListContainer = styled.ScrollView`
+  padding: 32px 24px;
+`;
+
+export const ProviderContainer = styled.TouchableOpacity<ProviderContainerPros>`
+  background: ${props => (props.selected ? '#ff9000' : '#3e3b47')};
+  flex-direction: row;
+  align-items: center;
+  padding: 8px 12px;
+  margin-right: 16px;
+  border-radius: 10px;
+`;
+
+export const ProviderAvatar = styled.Image`
+  width: 32px;
+  height: 32px;
+  border-radius: 16px;
+`;
+
+export const ProviderName = styled.Text<ProviderNamePros>`
+  margin-left: 8px;
+  font-family: 'RobotoSlab-Medium';
+  font-size: 16px;
+  color: ${props => (props.selected ? '#232129' : '#f4ede8')};
 `;
